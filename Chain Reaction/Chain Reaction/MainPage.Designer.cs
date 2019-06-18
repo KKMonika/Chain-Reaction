6353b50a8f9a1ac23938140b3b7db24867f10195
Davor <davor.dimoski@hotmail.com>
2019-06-18 16:01:09 +0200

    MainPage created

---
+++ Chain Reaction/Chain Reaction/MainPage.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Chain_Reaction
+{
+    partial class MainPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnInstructions = new System.Windows.Forms.Button();
+            this.btnCustomPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(262, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chain Reaction";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.Location = new System.Drawing.Point(266, 175);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(271, 56);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnInstructions
+            // 
+            this.btnInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstructions.Location = new System.Drawing.Point(266, 253);
+            this.btnInstructions.Name = "btnInstructions";
+            this.btnInstructions.Size = new System.Drawing.Size(271, 56);
+            this.btnInstructions.TabIndex = 3;
+            this.btnInstructions.Text = "Instructions";
+            this.btnInstructions.UseVisualStyleBackColor = true;
+            this.btnInstructions.Click += new System.EventHandler(this.btnInstructions_Click);
+            // 
+            // btnCustomPlay
+            // 
+            this.btnCustomPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomPlay.Location = new System.Drawing.Point(266, 329);
+            this.btnCustomPlay.Name = "btnCustomPlay";
+            this.btnCustomPlay.Size = new System.Drawing.Size(271, 56);
+            this.btnCustomPlay.TabIndex = 4;
+            this.btnCustomPlay.Text = "Custom Game";
+            this.btnCustomPlay.UseVisualStyleBackColor = true;
+            this.btnCustomPlay.Click += new System.EventHandler(this.btnCustomPlay_Click);
+            // 
+            // MainPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCustomPlay);
+            this.Controls.Add(this.btnInstructions);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.label1);
+            this.Name = "MainPage";
+            this.Text = "MainPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnInstructions;
+        private System.Windows.Forms.Button btnCustomPlay;
+    }
+}
